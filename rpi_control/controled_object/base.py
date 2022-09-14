859655861c6e9681b54bfbe0fbede1d6cae20151
AinaJ <rainanirina@gmail.com>
2022-09-14 06:18:28 +0200

    webapp

--- rpi_control/controled_object/base.py
+++ rpi_control/controled_object/base.py
@@ -1,12 +1,12 @@
 class ControledMeta(type):
     def __new__(cls, *args):
-        setattr(cls, "is_controlable", True)
+        setattr(cls, "is_controlable", True) # is_controlable attribute identify if it is contrable or not
         return super().__new__(cls, *args)
 
 
 class RaspObject(metaclass=ControledMeta):
     def __init__(self, channel):
-        self.channel = channel
+        self.channel = channel # a raspberry pi GPIO number where the object belong
 
     def __call__(self):
         pass
