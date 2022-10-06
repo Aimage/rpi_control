62c88297488c707028c17984447d054080ade810
AinaJ <rainanirina@gmail.com>
2022-10-06 21:07:55 +0200

    fix config gpio for led

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 from rpi_control.controled_object.light import Light
 import RPi.GPIO as GPIO
 
-
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(18, GPIO.OUT)
 
-TWILIO_SMS_CONTROLLED_OBJECTS = {
-        "GarageLight": Light(18, "GarageLight")
-        }
+TWILIO_SMS_CONTROLLED_OBJECTS = {"GarageLight": Light(18, "GarageLight")}
